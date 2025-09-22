fbd54a950f0a3f5549e05c2e5725425c311e48d0
yooseungyeon <ysy8317433@gmail.com>
2025-09-22 23:02:07 +0900

    initial

--- character_moves.py
+++ character_moves.py
@@ -6,6 +6,12 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 # 여기를 채우세요.
+for x in range(0,800,5):
+    clear_canvas()
+    grass.draw(400,30)
+    character.draw(x,90)
+    update_canvas()
+    delay(0.01)
 
 close_canvas()
 
--- character_runs.py
+++ character_runs.py
@@ -6,6 +6,16 @@ grass = load_image('grass.png')
 character = load_image('run_animation.png')
 
 # fill here
+frame =0
+for x in range(0,800,10): #10픽셀씩 이동
+    # #게임 렌더링
+    clear_canvas()
+    grass.draw(400,30)
+    character.clip_draw(frame * 100,0,100,100,x,130,200,200)
+    update_canvas()
+#게임로직
+    frame = (frame + 1) % 8
+    delay(0.05)
 
 
 close_canvas()
